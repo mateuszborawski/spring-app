a7b85c4f8d66a18db848ddc28f757a137f4528a3
mateuszborawski <mateusz.borawski.ama@gmail.com>
2022-03-27 13:38:58 +0200

    Added dynamic filtering

--- src/main/java/com/mateuszborawski/rest/webservices/restfulwebservices/beans/User.java
+++ src/main/java/com/mateuszborawski/rest/webservices/restfulwebservices/beans/User.java
@@ -1,9 +1,12 @@
 package com.mateuszborawski.rest.webservices.restfulwebservices.beans;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
+@JsonFilter("UserFilter")
 public class User {
     private Integer id;
 
--- src/main/java/com/mateuszborawski/rest/webservices/restfulwebservices/controllers/UserController.java
+++ src/main/java/com/mateuszborawski/rest/webservices/restfulwebservices/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.mateuszborawski.rest.webservices.restfulwebservices.controllers;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.mateuszborawski.rest.webservices.restfulwebservices.beans.User;
 import com.mateuszborawski.rest.webservices.restfulwebservices.exceptions.UserNotFoundException;
 import com.mateuszborawski.rest.webservices.restfulwebservices.services.UserDaoService;
@@ -9,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -22,12 +26,19 @@ public class UserController {
     private UserDaoService service;
 
     @GetMapping(path = "/users")
-    public List<User> retrieveAllUsers() {
-        return service.findAll();
+    public MappingJacksonValue retrieveAllUsers() {
+        MappingJacksonValue mapping = filterEntity(service.findAll(), "id", "name");
+        return mapping;
+    }
+
+    @GetMapping(path = "/users-filter")
+    public MappingJacksonValue retrieveAllUsersWithFiltering() {
+        MappingJacksonValue mapping = filterEntity(service.findAll(), "name", "birthDate");
+        return mapping;
     }
 
     @GetMapping(path = "/users/{id}")
-    public EntityModel<User> retrieveUser(@PathVariable int id) {
+    public MappingJacksonValue retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if(user == null) {
@@ -36,7 +47,8 @@ public class UserController {
 
         Link link = linkTo(methodOn(this.getClass()).retrieveAllUsers()).withRel("all-users");
 
-        return EntityModel.of(user, link);
+        MappingJacksonValue mapping = filterEntity(EntityModel.of(user, link), "name", "birthDate");
+        return mapping;
     }
 
     @PostMapping(path = "/users")
@@ -59,5 +71,15 @@ public class UserController {
         if(user == null) {
             throw new UserNotFoundException("User with id = " + id + " not found");
         }
+    }
+
+    private MappingJacksonValue filterEntity(Object entity, String... propertyArray) {
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(propertyArray);
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserFilter", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(entity);
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 }
